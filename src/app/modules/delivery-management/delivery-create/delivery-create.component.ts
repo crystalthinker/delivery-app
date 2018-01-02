5334c0ac06a10455ed8d25e771f35034fda99f5f
crystalthinker <abhilashkr555@gmail.com>
2018-01-02 12:09:27 +0530

    delivery module updates

--- src/app/modules/delivery-management/delivery-create/delivery-create.component.ts
+++ src/app/modules/delivery-management/delivery-create/delivery-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {DeliveryService} from '../../../core/services/delivery/delivery.service';
+import {ShopsService} from '../../../core/services/shops/shops.service';
 
 @Component({
   selector: 'app-delivery-create',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delivery-create.component.scss']
 })
 export class DeliveryCreateComponent implements OnInit {
-
-  constructor() { }
+  deliveryForm: FormGroup;
+  shops: any = [];
+  selectedShop: object = {};
+  constructor(private fb: FormBuilder, private service: DeliveryService, private shopService: ShopsService, private router: Router) {
+    this.createForm();
+  }
 
   ngOnInit() {
+    this.shopService.getAllShops().subscribe(shops => {
+      this.shops = shops;
+    });
+  }
+  createForm() {
+    /*    {
+      "id": "1",
+      "delivery_from": "vridhi",
+      "shop_id": "1",
+      "delivery_boy_id": "1",
+      "delivery_to": {
+        "customer_name": "",
+        "address": "",
+        "phone": ""
+      },
+      "delivery_boy_assigned": "Sebastin Eschweiler",
+      "status": "Awaiting pickup"
+    },*/
+    this.deliveryForm = this.fb.group({
+      shop_id: ['', [Validators.required]],
+    });
+  }
+  onShopChange(shopId) {
+    if ( shopId ) {
+      console.log('here', shopId);
+    }
+
+    // this.selectedShop = shop;
+  }
+  saveNewDelivery() {
+      console.log(this.deliveryForm.value);
+    // TODO Assign a delivery boy and create a delivery form format
+    // this.service.addNewDeliveryBoy(this.deliveryForm.value).subscribe(res => {
+      this.router.navigate(['/delivery/list']);
+    // });
   }
 
 }
