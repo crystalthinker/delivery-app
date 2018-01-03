d39a39b3080a757a8bc8ec44c381e6cc69207c28
crystalthinker <abhilashkr555@gmail.com>
2018-01-03 11:19:03 +0530

    moved breadcrumb to shared

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListTableComponent } from './list-table/list-table.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: [ListTableComponent],
-  exports: [ListTableComponent]
+  declarations: [ListTableComponent, BreadcrumbComponent],
+  exports: [ListTableComponent, BreadcrumbComponent]
 })
 export class SharedModule { }
